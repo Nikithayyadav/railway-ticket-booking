c1008fb5206904b7f6269965194aaf84841ededb
Nikitha yadav <CHANDAVENANIKITHA9@GMAIL.COM>
2026-08-11 18:46:25 +0530

    Update RailwayTicketBookingSystemApplicationTests.java

--- src/test/java/com/railway/booking/RailwayTicketBookingSystemApplicationTests.java
+++ src/test/java/com/railway/booking/RailwayTicketBookingSystemApplicationTests.java
@@ -2,7 +2,7 @@ package com.railway.booking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest; 
-
+ 
 @SpringBootTest
 class RailwayTicketBookingSystemApplicationTests { 
 
